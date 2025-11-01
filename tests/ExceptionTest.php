4d9a267ac063c16967df67dcba30a71fbfb5f550
Levan Velijanashvili <stichoza@gmail.com>
2025-11-02 02:45:28 +0400

    Update test URLs to use mock.httpstatus.io instead of httpstat.us

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -25,55 +25,55 @@ class ExceptionTest extends TestCase
     {
         $this->expectException(RateLimitException::class);
 
-        $this->tr->setUrl('https://httpstat.us/429')->translate('Test');
+        $this->tr->setUrl('https://mock.httpstatus.io/429')->translate('Test');
     }
 
     public function testRateLimitCaptchaException(): void
     {
         $this->expectException(RateLimitException::class);
 
-        $this->tr->setUrl('https://httpstat.us/503')->translate('Test');
+        $this->tr->setUrl('https://mock.httpstatus.io/503')->translate('Test');
     }
 
     public function testLargeTextException(): void
     {
         $this->expectException(LargeTextException::class);
 
-        $this->tr->setUrl('https://httpstat.us/413')->translate('Test');
+        $this->tr->setUrl('https://mock.httpstatus.io/413')->translate('Test');
     }
 
     public function testTranslationRequestException(): void
     {
         $this->expectException(TranslationRequestException::class);
 
-        $this->tr->setUrl('https://httpstat.us/418')->translate('Test');
+        $this->tr->setUrl('https://mock.httpstatus.io/418')->translate('Test');
     }
 
     public function testTranslationDecodingException(): void
     {
         $this->expectException(TranslationDecodingException::class);
 
-        $this->tr->setUrl('https://httpstat.us/200')->translate('Test');
+        $this->tr->setUrl('https://mock.httpstatus.io/200')->translate('Test');
     }
 
     public function testInheritanceForUnexpectedValueException(): void
     {
         $this->expectException(UnexpectedValueException::class);
 
-        $this->tr->setUrl('https://httpstat.us/200')->translate('Test');
+        $this->tr->setUrl('https://mock.httpstatus.io/200')->translate('Test');
     }
 
     public function testInheritanceForErrorException(): void
     {
         $this->expectException(ErrorException::class);
 
-        $this->tr->setUrl('https://httpstat.us/413')->translate('Test');
+        $this->tr->setUrl('https://mock.httpstatus.io/413')->translate('Test');
     }
 
     public function testLanguagesRequestException(): void
     {
         $this->expectException(LanguagesRequestException::class);
 
-        $this->tr->setUrl('https://httpstat.us/418')->languages();
+        $this->tr->setUrl('https://mock.httpstatus.io/418')->languages();
     }
 }
